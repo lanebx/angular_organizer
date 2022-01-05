bd1397c92927f9874e3f0a23b7dbc93f57250ac2
lanebx <havchikos@gmail.com>
2022-01-05 18:56:42 +0200

    start "calendar block"

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -1,4 +1,17 @@
+import { DateService } from './../shared/date.service';
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
+interface Day {
+  value: moment.Moment;
+  active: boolean;
+  disable: boolean;
+  selected: boolean;
+}
+
+interface Week {
+  days: Day[];
+}
 
 @Component({
   selector: 'app-organizer',
@@ -7,9 +20,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizerComponent implements OnInit {
 
-  constructor() { }
+  //calendar: Week[]
+
+  constructor(public dateService: DateService) { }
 
   ngOnInit(): void {
+    this.dateService.date.subscribe(this.generate.bind(this))
+  }
+
+  generate(now: moment.Moment) {
+    // console.log(now.clone().startOf('month').startOf('week').date);
+    const startDay = now.clone().startOf('month').startOf('week')
+    const endDay = now.clone().endOf('month').endOf('week')
+
+    const date = startDay.clone().subtract(1, 'day')
   }
 
 }
